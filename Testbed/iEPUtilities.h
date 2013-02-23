2e86cd77011caf16aca97daa79facf6894b3d745
Efthemios Suyat <quickfingersdev@gmail.com>
2013-02-22 23:18:14 -0500

    setting up utilities

---
+++ Testbed/iEPUtilities.h
@@ -0,0 +1,19 @@
+//
+//  iEPUtilities.h
+//  Testbed
+//
+//  Created by Efthemios Suyat on 2/22/13.
+//  Copyright (c) 2013 EfthemiosPrime. All rights reserved.
+//
+
+static NSString * const kUtilitiesVersion = @"1.1.2";
+static NSString * const kAlertTitle = @"BW RSS";
+static BOOL const kMessageActive = YES;
+
+// populated from loadDidView
+UITextView * messageTextView;
+
+void message ( NSString *format, ... );
+void alertMessage ( NSString *format, ... );
+NSString * flattenHTML ( NSString * html );
+NSString * trimString ( NSString * string );
